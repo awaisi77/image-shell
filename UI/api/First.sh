623626059f7d922c82b484c266914a2b41980a43
Awais <awaisullah@gosaas.io>
2021-08-21 09:26:22 +0000

    Update SH file to handle yarn & yarn dist build in container

--- UI/api/First.sh
+++ UI/api/First.sh
@@ -6,4 +6,11 @@ cp /home/hp/Dev/react_practice/image-shell/UI/api/storage/test/.env /home/hp/Dev
 
 cd /home/hp/Dev/react_practice/indivibook/indivibook-main
 ls -a
-docker run --rm -ti --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') --env ELECTRON_CACHE="/root/.cache/electron" --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" -v ${PWD}:/project  -v ${PWD##*/}-node-modules:/project/node_modules -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine
+OUTPUT=$(docker run -d --rm -ti --env-file <(env | grep -iE 'DEBUG|NODE_|ELECTRON_|YARN_|NPM_|CI|CIRCLE|TRAVIS_TAG|TRAVIS|TRAVIS_REPO_|TRAVIS_BUILD_|TRAVIS_BRANCH|TRAVIS_PULL_REQUEST_|APPVEYOR_|CSC_|GH_|GITHUB_|BT_|AWS_|STRIP|BUILD_') --env ELECTRON_CACHE="/root/.cache/electron" --env ELECTRON_BUILDER_CACHE="/root/.cache/electron-builder" -v ${PWD}:/project  -v ${PWD##*/}-node-modules:/project/node_modules -v ~/.cache/electron:/root/.cache/electron -v ~/.cache/electron-builder:/root/.cache/electron-builder electronuserland/builder:wine)
+
+docker ps
+
+echo "${OUTPUT}"
+
+docker exec -it "${OUTPUT}" sh -c "yarn && yarn dist-build"
+
